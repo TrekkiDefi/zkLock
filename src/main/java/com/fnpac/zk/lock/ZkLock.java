27655895a7396f1b26e673593fe94fc15477d11d
liuchunlong <631521383@qq.com>
2017-10-13 18:31:28 +0800

    :cloud:storage

--- src/main/java/com/fnpac/zk/lock/ZkLock.java
+++ src/main/java/com/fnpac/zk/lock/ZkLock.java
@@ -13,29 +13,88 @@ import org.apache.zookeeper.ZooDefs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
- * Created by 刘春龙 on 2017/10/12.
+ * Created by 刘春龙 on 2017/10/13.
+ * <p>
+ * eg，
+ * <p>
+ * 排他锁：/{namespace}/ExclusiveLock/{lockKey}/Lock
+ * <p>
+ * 共享锁：
+ * <p>
+ * 读：<br>
+ * /{namespace}/SharedLock/{lockKey}/RLock00000000<br>
+ * /{namespace}/SharedLock/{lockKey}/RLock00000001<br>
+ * /{namespace}/SharedLock/{lockKey}/RLock00000002<br>
+ * <p>
+ * 写：<br>
+ * /{namespace}/SharedLock/{lockKey}/WLock00000000<br>
  */
 public class ZkLock {
 
     private static final Logger logger = LoggerFactory.getLogger(ZkLock.class);
 
+    /**
+     * Global static variable
+     */
     private static CuratorFramework client = null;
     private static final String ExclusiveLock = "ExclusiveLock";// 排他锁目录
-    private static final String ShardLock = "ShardLock";// 共享锁目录
+    private static final String SharedLock = "SharedLock";// 共享锁目录
+    private static final String Lock = "Lock";
 
-    public ZkLock(String connectString, String namespace) {
+    /**
+     * Current lock variable
+     */
+    private final String lockKey;// 当前锁的Key
+    private CountDownLatch latch = new CountDownLatch(1);
+    private final Long timeout;// 超时等待时间
+
+    /**
+     * 构造锁对象
+     *
+     * @param connectString zookeeper连接地址
+     * @param namespace     命名空间
+     * @param lockKey       当前锁的Key
+     * @param timeout       超时等待时间
+     */
+    public ZkLock(String connectString, String namespace, String lockKey, Long timeout) {
         if (StringUtils.isEmpty(connectString)) {
-            throw new IllegalArgumentException("zookeeper connectString is Null");
+            throw new IllegalArgumentException("Zookeeper connectString is Null");
         }
         if (StringUtils.isEmpty(namespace)) {
-            throw new IllegalArgumentException("zookeeper namespace is Null");
+            throw new IllegalArgumentException("Zookeeper namespace is Null");
+        }
+        if (StringUtils.isEmpty(lockKey)) {
+            throw new IllegalArgumentException("Lock key is Null");
+        }
+        if (timeout == null) {
+            throw new IllegalArgumentException("Lock timeout is Null");
         }
 
+        this.lockKey = lockKey;
+        this.timeout = timeout;
+
+        // 创建zookeeper客户端连接，并初始化排他锁目录和共享锁目录
         init(connectString, namespace);
+
+        // 添加排他锁watcher
+        try {
+            addChildWatcher(ExclusiveLock, this.lockKey);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e.getCause());
+        }
     }
 
-    private void init(String connectString, String namespace) {
+    /**
+     * 创建zookeeper客户端连接，并初始化排他锁目录和共享锁目录
+     *
+     * @param connectString zookeeper连接地址
+     * @param namespace     命名空间
+     */
+    private static synchronized void init(String connectString, String namespace) {
         if (client != null)
             return;
 
@@ -54,23 +113,23 @@ public class ZkLock {
                         .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
                         .forPath("/" + ExclusiveLock);
             }
-            // 添加排他锁watcher
-            addChildWatcher("/" + ExclusiveLock);
 
+            // TODO 创建共享锁目录
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * 监听子节点的创建、删除、节点数据变化
+     * 监听当前锁的子节点的创建、删除、节点数据变化
      *
-     * @param path
+     * @param lockDir 锁目录
+     * @param lockKey 当前锁的Key
      * @throws Exception
      */
-    private void addChildWatcher(String path) throws Exception {
-        // 监听子节点的创建、删除、节点数据变化
-        final PathChildrenCache cache = new PathChildrenCache(client, path,
+    private void addChildWatcher(String lockDir, final String lockKey) throws Exception {
+        // 监听当前锁的子节点的创建、删除、节点数据变化
+        final PathChildrenCache cache = new PathChildrenCache(client, "/" + lockDir + "/" + lockKey,
                 true);
         cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
 
@@ -88,15 +147,14 @@ public class ZkLock {
                     // 有锁释放
                     String path = event.getData().getPath();
 
-                    if(path.startsWith("/" + ExclusiveLock)) {
-                        logger.info("排他锁,收到锁释放通知！锁：" + path);
+                    if (path.startsWith("/" + ExclusiveLock + "/" + lockKey)) {
+                        logger.info("排他锁,收到锁释放通知！path：" + path);
                         // 其它排他锁释放，countDown使当前线程重新去争夺锁（排他锁采用的是循环阻塞获取锁的方式）
-                        // 判断不是当前锁的释放
-                        // TODO
-                    } else if(path.startsWith("/" + ShardLock)) {
-                        logger.info("共享锁,收到锁释放通知！锁：" + path);
+                        latch.countDown();
+                    } else if (path.startsWith("/" + SharedLock + "/" + lockKey)) {
+                        logger.info("共享锁,收到锁释放通知！path：" + path);
                         // 其它共享锁释放，重新去争夺锁，如果成功获取锁，则countDown使当前线程返回获取锁成功（共享锁采用的是阻塞获取锁的方式）
-                        // 判断不是当前锁的释放
+                        // 判断是不是当前锁的释放
                         // TODO
                     }
                 } else if (event.getType().equals(
@@ -105,5 +163,40 @@ public class ZkLock {
                 }
             }
         });
+    }
+
+    /**
+     * 获取排他锁
+     * <p>
+     * 排他锁采用的是循环阻塞获取锁的方式
+     */
+    public boolean getExclusiveLock() {
+        while (true) {
+            try {
+                client.create().creatingParentsIfNeeded()
+                        .withMode(CreateMode.EPHEMERAL)
+                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                        .forPath("/" + ExclusiveLock + "/" + this.lockKey + "/" + Lock);
+
+                logger.info("获取排他锁成功");
+                return true;// 如果节点创建成功，即说明获取排他锁成功，返回true
+            } catch (Exception e) {
+                logger.info("获取排他锁失败");
+                // 如果没有获取到排他锁，需要重新设置同步资源值
+                if (latch.getCount() <= 0) {
+                    latch = new CountDownLatch(1);
+                }
+                try {
+                    boolean rs = latch.await(this.timeout, TimeUnit.MILLISECONDS);
+                    if (!rs) {// false，等待超时
+                        logger.info("获取排他锁等待超时，获取锁失败!~");
+                        return false;
+                    }
+                } catch (InterruptedException ie) {
+                    logger.error("线程中断，获取排他锁失败", ie);
+                    return false;
+                }
+            }
+        }
     }
 }
